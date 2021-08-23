d8160153da055eaf487d0a3ea94758f5f05f94fe
Haochuan <gohcsu@gmail.com>
2021-08-23 18:57:20 +0800

    Add docker login

--- hack/build-deploy-app.sh
+++ hack/build-deploy-app.sh
@@ -12,6 +12,8 @@ unzip DB11.ZIP -d DB11
 mv DB11/IP2LOCATION-LITE-DB11.IPV6.BIN DB11.BIN
 rm -rf DB11.ZIP DB11
 
+docker login --username=sunfmin --password=$DOCKER_HUB_PASSWORD
+
 export IMAGE_TAG="$(date "+%Y%m%d")"
 
 plantbuild push ./plantbuild/build.jsonnet -v "$IMAGE_TAG"
