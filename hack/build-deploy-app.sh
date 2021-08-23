7fdef57f1bae48159f561aa589ad4acc070e6f46
Haochuan <gohcsu@gmail.com>
2021-08-23 17:24:49 +0800

    Rename test country code var

--- hack/build-deploy-app.sh
+++ hack/build-deploy-app.sh
@@ -18,9 +18,9 @@ plantbuild push ./plantbuild/build.jsonnet -v "$IMAGE_TAG"
 
 ./hack/deploy-to-cluster.sh test "ssh -o StrictHostKeychecking=no ubuntu@bastion.test.shared.k8s.theplant.dev /bin/bash"
 
-TEST_VALIDATION=$(curl -I -m 3 -s -H 'IP2Location-IP: 1.1.1.1' -i https://ip2location-test.theplant-dev.com | awk 'BEGIN {FS=": "}/^ip2location-country-code/{print $2}')
+TEST_COUNTRY_CODE=$(curl -I -m 3 -s -H 'IP2Location-IP: 1.1.1.1' -i https://ip2location-test.theplant-dev.com | awk 'BEGIN {FS=": "}/^ip2location-country-code/{print $2}')
 
-if [ "$TEST_VALIDATION" == "US" ]; 
+if [ "$TEST_COUNTRY_CODE" == "US" ]; 
   then
     ./deploy-to-cluster.sh prod "ssh -o StrictHostKeychecking=no ubuntu@bastion.prod.aigle.k8s.theplant.dev /bin/bash"
   else
