0bc9e4b4b66d83cce62a76a36e14f9042a2b995d
Haochuan <gohcsu@gmail.com>
2021-08-30 17:39:01 +0800

    CI: debug ci

--- hack/deploy-to-cluster.sh
+++ hack/deploy-to-cluster.sh
@@ -6,6 +6,9 @@ set -o pipefail
 CLUSTER=${1}
 KUBECTL_BASH=${2}
 
+echo "$CLUSTER"
+echo "$KUBECTL_BASH"
+
 # if no cluster aliases provided, display the usage and exit
 if [ "$CLUSTER" == "" ]; then
   echo "Usage: CLUSTER=test or prod $0"
@@ -23,22 +26,40 @@ else
   # export KUBECTL_BASH="ssh -o StrictHostKeychecking=no ubuntu@bastion.foobar.theplant.dev /bin/bash"
 fi
 
+echo 1
+echo "$KUBECTL_BASH"
+
+echo 2
+echo "$KUBECTL_BASH"
+
 # allows CI presubmit jobs to test by dry-running
 if [ "$JOB_TYPE" == "periodics" ]; then
   plantbuild k8s_apply ./plantbuild/"$CLUSTER"/all.jsonnet -v "$IMAGE_TAG" -d client
   plantbuild k8s_apply ./plantbuild/"$CLUSTER"/all.jsonnet -v "$IMAGE_TAG" -d server
 fi
 
+echo 3
+echo "$KUBECTL_BASH"
+
 plantbuild k8s_apply ./plantbuild/"$CLUSTER"/all.jsonnet -v "$IMAGE_TAG"
+
+echo 4
+echo "$KUBECTL_BASH"
 
 # consider to move this functionality to plantbuild
 if [ -z "$KUBECTL_BASH" ]; then
   KUBECTL_BASH="/bin/bash"
 fi
 
+echo 5
+echo "$KUBECTL_BASH"
+
 NAMESPACE="ip2location-$CLUSTER"
 COMMAND="kubectl -n $NAMESPACE get deploy -o name |
           xargs -n1 \
             kubectl -n $NAMESPACE rollout status --timeout 150s"
+
+echo 6
+echo "$KUBECTL_BASH"
 
 echo "$COMMAND" | $KUBECTL_BASH
