22c4387c294cf9866a01bee9f70842547e1b3ba0
Haochuan <gohcsu@gmail.com>
2021-08-30 21:20:31 +0800

    CI: debug

--- hack/build-deploy-app.sh
+++ hack/build-deploy-app.sh
@@ -28,10 +28,10 @@ plantbuild push ./plantbuild/build.jsonnet -v "$IMAGE_TAG"
 
 ./hack/deploy-to-cluster.sh test "$SHARED_TEST_CLUSTER"
 
-TEST_COUNTRY_CODE=$(curl -I -m 3 -s -H 'IP2Location-IP: 1.1.1.1' -i https://ip2location-test.theplant-dev.com | awk 'BEGIN {FS=": "}/^ip2location-country-code/{print $2}')
-export TEST_COUNTRY_CODE
+# TEST_COUNTRY_CODE=$(curl -I -m 3 -s -H 'IP2Location-IP: 1.1.1.1' -i https://ip2location-test.theplant-dev.com | awk 'BEGIN {FS=": "}/^ip2location-country-code/{print $2}')
+TEST_COUNTRY_CODE=$(curl -I -m 3 -s -H 'IP2Location-IP: 1.1.1.1' -i https://ip2location-test.theplant-dev.com | awk 'BEGIN {FS=": "}/^ip2location-region/{print $2}')
 
-if [ "$TEST_COUNTRY_CODE" == "US" ]; 
+if [ "$TEST_COUNTRY_CODE" == "California" ]; 
   then
     echo "Test cluster deploy successfully, TEST_COUNTRY_CODE = $TEST_COUNTRY_CODE"
     ./hack/deploy-to-cluster.sh prod "$SHARED_K8S_CLUSTER"
