2d79fcbdc969ae53b1006f07c815444cb3db7737
Haochuan <gohcsu@gmail.com>
2021-09-01 12:08:49 +0800

    CI: debug

--- hack/build-app.sh
+++ hack/build-app.sh
@@ -10,7 +10,7 @@ IMAGE_TAG=${1}
 source hack/get-tools.sh
 
 # curl -fSL -o DB11.ZIP "http://www.ip2location.com/download/?token=${DOWNLOAD_TOKEN}&file=DB11LITEBINIPV6"
-curl -fSL -o DB11.ZIP "https://db11.s3.amazonaws.com/DB11.BIN.zip"
+curl -fSL -o DB11.ZIP "https://db11.s3.amazonaws.com/IP2LOCATION-LITE-DB11.IPV6.BIN.ZIP"
 unzip DB11.ZIP -d DB11
 mv DB11/IP2LOCATION-LITE-DB11.IPV6.BIN DB11.BIN
 rm -rf DB11.ZIP DB11
