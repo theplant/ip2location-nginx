ea1b610951bb7af58a3018f4062016416082f1a2
Haochuan <gohcsu@gmail.com>
2021-09-01 12:21:03 +0800

    CI: remove debug command

--- hack/build-app.sh
+++ hack/build-app.sh
@@ -9,8 +9,7 @@ IMAGE_TAG=${1}
 # shellcheck disable=SC1091
 source hack/get-tools.sh
 
-# curl -fSL -o DB11.ZIP "http://www.ip2location.com/download/?token=${DOWNLOAD_TOKEN}&file=DB11LITEBINIPV6"
-curl -fSL -o DB11.ZIP "https://db11.s3.amazonaws.com/IP2LOCATION-LITE-DB11.IPV6.BIN.ZIP"
+curl -fSL -o DB11.ZIP "http://www.ip2location.com/download/?token=${DOWNLOAD_TOKEN}&file=DB11LITEBINIPV6"
 unzip DB11.ZIP -d DB11
 mv DB11/IP2LOCATION-LITE-DB11.IPV6.BIN DB11.BIN
 rm -rf DB11.ZIP DB11
